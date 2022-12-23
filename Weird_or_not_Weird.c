5bf5d4f9929912ab97949524ab7d652dc8b52fbc
22A91A04M2 <119721144+22A91A04M2@users.noreply.github.com>
2022-12-23 23:50:08 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x%2!=0||x>=6&&x<=20)     {         printf("weird");     }     else     {         printf("not weird");     } }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x%2!=0||x>=6&&x<=20)
+    {
+        printf("weird");
+    }
+    else
+    {
+        printf("not weird");
+    }
+}
